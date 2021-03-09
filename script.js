e8ba760fc2042a6945cb1bb0f3d2ff70284c55a2
Francis N <gundammerlin@gmail.com>
2021-03-09 13:19:29 -0600

    Updated

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ function renderData(movieData) {
   movieClass.appendChild(movieYear)
   movieClass.appendChild(movieDirector)
   movieClass.appendChild(movieDescription)
+    function removeData ()
 // //where to add the remove method?  
 }
 async function chooseMovie(event) {
