e50c26bab376367b852908ad0acc95753a9b020d
Francis N <gundammerlin@gmail.com>
2021-03-09 15:23:40 -0600

    Updated 3:23pm

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//target
+let currentlyDisplayedMovies = [];
 const movieList = document.querySelector("select");
 function renderOptions(movies) {
   movies.forEach(movie => {
@@ -19,33 +19,48 @@ async function handleRequest() {
 }
 handleRequest();
 const movieClass = document.querySelector(".movieinfo")
-function renderData(movieData) {
-  console.log(movieData);
+function renderData() {
+  movieClass.innerHTML = ""
+  if (currentlyDisplayedMovies.length >= 5) {
+    currentlyDisplayedMovies.pop();
+  }
+  currentlyDisplayedMovies.forEach(movie => { 
   let movieYear = document.createElement('h2')
   let movieDirector = document.createElement('h3')
   let movieDescription = document.createElement('p')
-  movieYear.innerText = movieData.release_date
-  movieDirector.innerText = movieData.director
-  movieDescription.innerText = movieData.description
+  movieYear.innerText = movie.release_date
+  movieDirector.innerText = movie.director
+  movieDescription.innerText = movie.description
   movieClass.appendChild(movieYear)
   movieClass.appendChild(movieDirector)
-  movieClass.appendChild(movieDescription)
+    movieClass.appendChild(movieDescription)
+    let imageDiv = document.createElement('img')
+    let displayImage;
+  for (let key in images) {
+    if (movie.id === key) {
+      displayImage = images[key].image
+    }
+  }
+    imageDiv.src = displayImage;
+    movieClass.appendChild(imageDiv);
+  })   
 }
 async function chooseMovie(event) {
   try {
     const url = `https://ghibliapi.herokuapp.com/films/${event.target.value}`
     const response = await axios.get(url);
-    renderData(response.data);
-    // console.log(response);
+      currentlyDisplayedMovies = [response.data,...currentlyDisplayedMovies]
+      renderData();
+    console.log(response);
   } catch (err) {
-    console.log('This is not Working');
+    console.log(err.message)
+    // console.log('This is not Working');
     // console.log(err)
   }
 }
 movieList.addEventListener('change', chooseMovie)
 
 
-  
 
 
 
